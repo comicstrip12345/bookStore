bd6f27f16c59717345a44f1b4492ea9eaa0b6614
comicstrip12345 <sabanalers@gmail.com>
2022-08-25 23:33:42 +0800

    Revert "Initial Commit"
    This reverts commit d88ef8630289c3d22fdcf91b9c27a52c412171a9.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux/es/exports';
-import store from './components/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    
+    <App />
   </React.StrictMode>
 );
 
